8159dbf00125d535b597fe463593f4203b93905a
kahohonda917 <g1720538@is.ocha.ac.jp>
2021-04-05 15:57:11 +0900

    qr-delete

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -100,9 +100,9 @@
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
-            <div id="result">
+            <!-- <div id="result">
                 <video></video>
-            </div>
+            </div> -->
             <!-- <p id="latitude"></p>
             <p id="longitude"></p> -->
             <form method="POST" id="edit-form">
@@ -270,30 +270,30 @@
             calendar.render();
         });
 
-        const constraints = { audio: false, video: { facingMode: 'environment', width: 150, height: 150 }};
+        // const constraints = { audio: false, video: { facingMode: 'environment', width: 150, height: 150 }};
 
-        navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
-            const video = document.querySelector('video');
-            video.srcObject = stream;
-            video.play();
+        // navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
+        //     const video = document.querySelector('video');
+        //     video.srcObject = stream;
+        //     video.play();
 
-            const w = constraints.video.width, h = constraints.video.height;
-            const canvas = document.createElement('canvas');
-            canvas.width = w;
-            canvas.height = h;
-            const context = canvas.getContext('2d');
+        //     const w = constraints.video.width, h = constraints.video.height;
+        //     const canvas = document.createElement('canvas');
+        //     canvas.width = w;
+        //     canvas.height = h;
+        //     const context = canvas.getContext('2d');
 
-            const timer = setInterval(() => {
-                context.drawImage(video, 0, 0, w, h);
-                const imageData = context.getImageData(0, 0, w, h);
-                const code = jsQR(imageData.data, imageData.width, imageData.height);
-                if (code) {
-                    clearInterval(timer);
-                    document.querySelector('#result').textContent = code.data;
-                }
-            }, 500);
-        }).catch((e) => {
-            console.log('load error', e);
-        });
+        //     const timer = setInterval(() => {
+        //         context.drawImage(video, 0, 0, w, h);
+        //         const imageData = context.getImageData(0, 0, w, h);
+        //         const code = jsQR(imageData.data, imageData.width, imageData.height);
+        //         if (code) {
+        //             clearInterval(timer);
+        //             document.querySelector('#result').textContent = code.data;
+        //         }
+        //     }, 500);
+        // }).catch((e) => {
+        //     console.log('load error', e);
+        // });
     </script>
 @endsection
